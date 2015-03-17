3bf19d9225ebd0cfe41f427c7d6695a1e7fe489d
Angela Bier <abier@corelando.com>
2015-03-16 20:49:30 -0400

    add after_action, update action, destroy action and secure_params to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,38 @@
 class UsersController < ApplicationController
-    before_filter :authenticate_user!
+  before_filter :authenticate_user!
+  after_action :verify_authorized
 
-    def index
-      unless current_user.admin?
-        redirect_to :back, :alert => "Access denied."
-      end
-      @users = User.all
-    end
+  def index
+    @users = User.all
+    authorize User
+  end
 
-    def show
-      @user = User.find(params[:id])
-      unless current_user.admin?
-        unless @user == current_user
-          redirect_to :back, :alert => "Access denied."
-        end
-      end
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
     end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    authorize user
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
+  end
+
+  private
+
+  def secure_params
+    params.require(:user).permit(:role)
+  end
+
 end
