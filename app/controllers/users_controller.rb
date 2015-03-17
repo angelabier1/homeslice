c53ee810c33712bf1ff4cb006f76c18fb2f95612
Angela Bier <abier@corelando.com>
2015-03-16 20:47:17 -0400

    add index and show actions to UsersController. Allow access to index action only if current_user is an admin. Restrict access to show action; only allow access if current_user is an admin or if profile belongs to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+    before_filter :authenticate_user!
 
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
+    def index
+      unless current_user.admin?
+        redirect_to :back, :alert => "Access denied."
+      end
+      @users = User.all
     end
-  end
 
+    def show
+      @user = User.find(params[:id])
+      unless current_user.admin?
+        unless @user == current_user
+          redirect_to :back, :alert => "Access denied."
+        end
+      end
+    end
 end
