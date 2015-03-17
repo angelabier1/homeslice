54289c1e887d337123efde07997898bc778f1b7b
Angela Bier <abier@corelando.com>
2015-03-16 20:40:29 -0400

    add ActiveRecord enum for role to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :client, :agent, :broker, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def role?(r)
-    role.include? r.to_s
-  end
+
 end
