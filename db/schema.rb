d75b7306e98aa15702b86b52af3c3a67298b6ddd
Angela Bier <abier@corelando.com>
2015-03-16 20:36:15 -0400

    change :role from :string to :integer in :users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316070936) do
+ActiveRecord::Schema.define(version: 20150317003527) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20150316070936) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.string   "role"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
